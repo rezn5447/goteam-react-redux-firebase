2ff1d2d601effb2dc8f581a175e9970a3251e3cb
Resdan Logan <rezn5447@gmail.com>
2017-01-23 08:49:41 -0800

    Containers hold forms now

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import * as actions from '../actions/auth';
-import LoginForm  from '../forms/LoginForm';
+import { Field, reduxForm } from 'redux-form';
+import FontAwesome from 'react-fontawesome';
 
 class LoginContainer extends Component {
   handleSubmit = (values) => {
     this.props.signIn(values)
   }
+
   renderAuthenticationError() {
-  if (this.props.authenticationError) {
-    return <div className="alert alert-danger">{ this.props.authenticationError }</div>;
+    if (this.props.authenticationError) {
+      return <div className="alert alert-danger">{ this.props.authenticationError }</div>;
+    }
+      return <div></div>;
   }
-  return <div></div>;
-}
+  
   render () {
     return (
       <div className="login-page-container">
-        <LoginForm onSubmit={this.handleSubmit} />
+        <div className="login-form">
+          <h1> Login </h1>
+          <form onSubmit={this.handleSubmit}>
+            <FontAwesome className="super-crazy-colors" size="2x" name="user" />
+            <Field className="form-control" component="input" type="email" name="email" placeholder="Email"/><br/>
+            <FontAwesome className="fa fa-lock" name="lock" aria-hidden="true" />
+            <Field className="form-control" component="input" type="password" name="pw" placeholder="Password"/><br/>
+            <button type="submit" className="btn btn-primary">Login</button>
+          </form>
+        </div>
       </div>
-    )
+    );
   }
 }
-
-export default connect(null, actions)(LoginContainer);
+export default reduxForm({
+    form: 'login'
+})(LoginContainer)
