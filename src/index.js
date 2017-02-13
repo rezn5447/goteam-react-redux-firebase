17b53f134d4fb5194c95f1cc1d0112e54184b100
Resdan Logan <rezn5447@gmail.com>
2017-02-13 14:07:08 -0800

    refactoring changes

--- src/index.js
+++ src/index.js
@@ -11,15 +11,8 @@ import sportReducer from './reducers/sport_reducer';
 import authReducer from './reducers/auth_reducer';
 import MyRouter from './router/Router';
 
-import FontAwesome from 'react-fontawesome';
-import './assets/stylesheets/index.scss';
-import './assets/stylesheets/main_layout.scss';
-import './assets/stylesheets/home.scss';
-import './assets/stylesheets/matches.scss';
-import './assets/stylesheets/score.scss';
-import './assets/stylesheets/sports.scss';
-import './assets/stylesheets/users.scss';
 
+import './assets/stylesheets/index.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
@@ -28,7 +21,7 @@ const reducers = combineReducers({
   form: reduxFormReducer, // mounted under "form"
   user: userReducer,
   sports: sportReducer,
-  auth: authReducer
+  authenticated: authReducer
 })
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
@@ -44,4 +37,4 @@ ReactDOM.render(
 
 // setup Firebase listeners
 
- // store.dispatch(actions.verifyAuth())
+ store.dispatch(actions.verifyAuth())
